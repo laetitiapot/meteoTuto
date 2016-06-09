d1eb2251113f5b12d660c8535de916aeb948d010
Laetitia <potlaetitia@gmail.com>
2016-06-09 18:13:37 +0200

    Update3

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,3 +1,4 @@
+// Datas weather
 var weather = {
   "coord": {
     "lon": 4.85,
@@ -42,49 +43,45 @@ var weather = {
   "name": "Lyon",
   "cod": 200
 };
-console.log(weather);
-alert(weather.weather);
-// Arrondis
+//console.log(weather);
+
+
+// Datas Arrondis
 function Round(x){
 	if (typeof x === "number") {
 		
 		return Math.round(x);
 	}
 };
-var temp = weather.main.temp;
-temp = Round(temp);
-document.write(temp);
 
-/*
-var temp = Math.round(weather.main.temp);
-var description = Math.round(weather.main.temp);
-var tempMin = Math.round(weather.main.temp_min);
-var tempMax = Math.round(weather.main.temp_max);
-var wind = Math.round(weather.wind.speed);
-var pressure = Math.round(weather.main.pressure);
-*/
-
-
+// Var datas
+var temp = Round(weather.main.temp);
+var description = Round(weather.weather[0].description);
+var tempMin = Round(weather.main.temp_min);
+var tempMax = Round(weather.main.temp_max);
+var wind = Round(weather.wind.speed);
+var humidity = Round(weather.main.humidity);
 var city = weather.name;
 var country = weather.sys.country;
 
-
-
-/*var date = new Date();
+// Hours / Date
+var date = new Date();
 var hours = date.getHours() ; 
 var minutes = date.getMinutes()
 var day = date.getDate();
 var month = date.getMonth()+1;
 var year = date.getFullYear();
-var infosDate = hours + " : " + minutes + " " +  day + "/" + month + "/" + year ;*/
+var infosDate = hours + " : " + minutes + " " +  day + "/" + month + "/" + year ;
 
 window.onload = function infos(){
-	//document.getElementById("day").innerHTML = infosDate ;
-	//document.getElementById("temp").innerHTML = temp ;
-	//document.getElementById("minMax").innerHTML = tempMin + '°C / ' + tempMax + '°C';
-	//document.getElementById("wind").innerHTML = wind;
-	//document.getElementById("pressure").innerHTML = pressure;
+	document.getElementById("day").innerHTML = infosDate ;
+	document.getElementById("temp").innerHTML = temp ;
+	document.getElementById("minMax").innerHTML = 'Temp min. ' + tempMin + '°C / ' + 'max. ' +tempMax + '°C';
+	document.getElementById("wind").innerHTML = 'Vent : ' + wind;
+	document.getElementById("humidity").innerHTML = 'Humidité : ' + humidity;
 	document.getElementById("city").innerHTML = "<span class='glyphicon glyphicon-map-marker'></span> " + city + ', ' + country ;
 	
 
 };
+
+console.log(weather.weather[0].description);
