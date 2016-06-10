92a6251a01b07d7d54eac027701637e6f20067b9
Laetitia <potlaetitia@gmail.com>
2016-06-10 11:05:18 +0200

    V4

--- javascripts/app.js
+++ javascripts/app.js
@@ -6,38 +6,36 @@ var weather = {
   },
   "weather": [
     {
-      "id": 800,
-      "main": "Clear",
-      "description": "clear sky",
-      "icon": "01d"
+      "id": 803,
+      "main": "Clouds",
+      "description": "broken clouds",
+      "icon": "04d"
     }
   ],
   "base": "stations",
   "main": {
-    "temp": 291.41,
-    "pressure": 1017,
-    "humidity": 71,
-    "temp_min": 290.37,
-    "temp_max": 293.15
+    "temp": 20.06,
+    "pressure": 1013,
+    "humidity": 53,
+    "temp_min": 18.33,
+    "temp_max": 23.89
   },
   "wind": {
-    "speed": 4.11,
-    "gust": 7.2
+    "speed": 1.69,
+    "deg": 37.0041
   },
-  "rain": {
-    "3h": 0.005
-  },
+  "rain": {},
   "clouds": {
-    "all": 0
+    "all": 56
   },
-  "dt": 1465460821,
+  "dt": 1465544230,
   "sys": {
     "type": 3,
     "id": 62901,
-    "message": 0.0313,
+    "message": 0.0284,
     "country": "FR",
-    "sunrise": 1465444253,
-    "sunset": 1465500564
+    "sunrise": 1465530641,
+    "sunset": 1465586998
   },
   "id": 2996944,
   "name": "Lyon",
@@ -46,12 +44,28 @@ var weather = {
 //console.log(weather);
 
 
-// Datas Arrondis
+// Function datas rounded to the nearest integer
 function Round(x){
 	if (typeof x === "number") {
 		
 		return Math.round(x);
 	}
+};
+// Function formatting date and hours
+function formattedDate(date) {
+    var d = new Date(date || Date.now()),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+        hours = d.getHours(); 
+        minutes = d.getMinutes() ;
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+    if (hours.length < 2) hours = '0' + hours;
+    if (minutes.length < 2) minutes = '0' + minutes;
+    var infosDate = '<div id ="hours"> '+ hours + ' : '+ minutes +'</div> '+ day +"/" + month +"/" + year ;
+    return infosDate ;
 };
 
 // Var datas
@@ -63,25 +77,17 @@ var wind = Round(weather.wind.speed);
 var humidity = Round(weather.main.humidity);
 var city = weather.name;
 var country = weather.sys.country;
+// var date and hours
+var infosDate = formattedDate();
 
-// Hours / Date
-var date = new Date();
-var hours = date.getHours() ; 
-var minutes = date.getMinutes()
-var day = date.getDate();
-var month = date.getMonth()+1;
-var year = date.getFullYear();
-var infosDate = hours + " : " + minutes + " " +  day + "/" + month + "/" + year ;
-
+//
 window.onload = function infos(){
 	document.getElementById("day").innerHTML = infosDate ;
-	document.getElementById("temp").innerHTML = temp ;
+	document.getElementById("temp").innerHTML = temp + '°C  ' ;
 	document.getElementById("minMax").innerHTML = 'Temp min. ' + tempMin + '°C / ' + 'max. ' +tempMax + '°C';
-	document.getElementById("wind").innerHTML = 'Vent : ' + wind;
-	document.getElementById("humidity").innerHTML = 'Humidité : ' + humidity;
+	document.getElementById("wind").innerHTML = "<span class='glyphicon glyphicon-flag'></span> Vent : " + wind + ' m/s';
+	document.getElementById("humidity").innerHTML = "<span class='glyphicon glyphicon-tint'></span> Humidité : " + humidity + ' %';
 	document.getElementById("city").innerHTML = "<span class='glyphicon glyphicon-map-marker'></span> " + city + ', ' + country ;
 	
 
 };
-
-console.log(weather.weather[0].description);
